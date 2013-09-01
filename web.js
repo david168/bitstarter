71bf3d29cd63479931aaa0b306bee932dd71a260
David Lai <dlai99@yahoo.com>
2013-09-01 17:54:40 +0000

    Change to read content from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World from index.html - David Lai');
+  var contents = fs.readFile('index.hhml');
+  response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
