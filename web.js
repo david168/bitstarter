716d6fd5e088f886e8480d2e2a5a96b2d03ab483
David Lai <dlai99@yahoo.com>
2013-09-01 17:13:10 +0000

    Change Comment

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World from index.html');
+  response.send('Hello World from index.html - David Lai');
 });
 
 var port = process.env.PORT || 5000;
